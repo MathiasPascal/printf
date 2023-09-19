0a7f167a50fbcb158b33a58b761fa3e95f9c87d2
amoni kevin <kevinyamatta50@gmail.com>
2023-09-19 11:43:34 -0700

    new

--- printf.c
+++ printf.c
@@ -6,11 +6,11 @@
  */
 int _printf(const char *format, ...)
 {
-	int length = 0, i, j;
+	int i, j, length = 0;
 	va_list our_list;
 
 	print list[] = {
-		{"c", print_char}, {"s", print_str}, {"%", print_percent}, {"d", print_int}, {"u", print_unsigned}, {"i", print_int}, {"b", print_binary1}, {"S", print_str}
+		{"c", print_char}, {"s", print_str}, {"%", print_percent}, {"d", print_int}, {"u", print_unsigned}, {"i", print_int}, {"b", bc}, {"S", print_str}
 	};
 	va_start(our_list, format);
 	if (!format)
@@ -19,26 +19,26 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			write(1, format, 1);
+			_putchar(format[i]);
 			length++;
 		}
 		else
 		{
 			if (!format[i + 1] || format[i + 1] == ' ')
 				return (-1);
-			for (j = 0; j < 8; j++)
+			for (j = 0; j < 6; j++)
 			{
 				if (format[i + 1] == *(list[j].count))
 					break;
 			}
-			if (j < 8)
+			if (j < 6)
 			{
 				length = length + list[j].func_ptr(our_list);
 				i++;
 			}
 			else
 			{
-				write(1, format, 1);
+				_putchar(format[i]);
 				length++;
 			}
 		}
